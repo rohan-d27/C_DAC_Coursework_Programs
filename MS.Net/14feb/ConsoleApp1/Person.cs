7d0ca216fd007a2ee35a6c4df9ef39ecf8a557c1
Rohan  Dharurkar <89681077+rohan-d27@users.noreply.github.com>
2022-03-05 19:33:09 +0530

    Add files via upload

---
+++ MS.Net/14feb/ConsoleApp1/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR
+{
+    class Person
+    {
+        private String _firstName, _lastName, _email;
+        private int _contactNumber;
+        public static int _count;
+        public Person()//parameterless constructor
+        {
+            _count++;
+        }
+
+        public Person(string _firstName, string _lastName, string _email, int _contactNumber)//parameterized constructor
+        {
+            this._firstName = _firstName;
+            this._lastName = _lastName;
+            this._email = _email;
+            this._contactNumber = _contactNumber;
+            _count++;
+        }
+        public String FirstName //getter setter for first name
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public String LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public String Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+        public int ContactNumber
+        {
+            get
+            {
+                return _contactNumber;
+            }
+            set
+            {
+                _contactNumber = value;
+            }
+        }
+
+        public override string ToString()//ToString Method
+        {
+            return "First Name: " + this.FirstName + ", " + "Last sName: " + this.LastName + ", " + "Email: " + this.Email + ", " + "Contact Number: " + this.ContactNumber;
+        }
+
+    }
+}
---
+++ MS.Net/14feb/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World!");
+            Person p1 = new Person("a", "b", "ab@gmail.com", 1234343);
+            Console.WriteLine(" First Name: " + p1.FirstName + ", " + "Last sName: " + p1.LastName + ", " + "Email: " + p1.Email + ", " + "Contact Number: " + p1.ContactNumber);
+            Console.WriteLine("\n From To String Method: " + p1);
+            Person p2 = new Person();
+            Person p3 = new Person();
+            Console.WriteLine("\n Count: " + Person._count);
+            Console.ReadLine();
+        }
+    }
+}
